8e021918b7799b657b7da7056062cc40aa609a10
tgyuu-An <atg0614@gmail.com>
2022-10-30 00:01:46 +0900

    third commit

--- Network(BFS)_python.py
+++ Network(BFS)_python.py
@@ -1,6 +1,23 @@
-a = [1,2,3,4,5,6,7]
+from collections import deque
 
-for i in a:
-    print(i)
-    print(a)
-    a.pop(0)
+def solution(n, computers):
+    answer = 0
+    visited = [False for _ in range(n)]
+    
+    for i in range(n):
+        if visited[i] == True:
+            continue
+            
+        else:
+            visited[i] = True
+            queue = deque(computers[i])
+            idx=0
+            while queue:
+                if idx>=n:
+                    idx-=n
+                if queue.popleft()==1 and visited[idx]==False:
+                    visited[idx]=True
+                    queue.extend(computers[idx])
+                idx +=1
+            answer +=1
+    return answer
