43b158abe393c17bac07978ad8b0c3895d6df954
tgyuu-An <atg0614@gmail.com>
2024-01-13 20:58:06 +0900

    BOJ 17485 진우의 달 여행 - DP

--- BOJ/그리디/2212_센서_python.py
+++ BOJ/그리디/2212_센서_python.py
@@ -14,13 +14,13 @@ else:
     for idx in range(1,len(sensor_set)):
         heappush(gap_table,(-(sensor_set[idx]-sensor_set[idx-1])))
     
-    print(sensor_set)
-    print(gap_table)
-    print(total_distance)
+    # print(sensor_set)
+    # print(gap_table)
+    # print(total_distance)
     
     for _ in range(concentrate_count-1):
         distance = heappop(gap_table)
         total_distance += distance
     
-    print(gap_table)
+    # print(gap_table)
     print(total_distance)
