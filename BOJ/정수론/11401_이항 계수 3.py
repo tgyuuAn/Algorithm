6f5ffee62b59ef69937b88413e3ca6fd3b0ad898
tgyuu-An <atg0614@gmail.com>
2024-01-29 00:56:27 +0900

    BOJ 11401 이항 계수 3 - 정수론

---
+++ BOJ/정수론/11401_이항 계수 3.py
@@ -0,0 +1,25 @@
+N, K = map(int,input().split())
+    
+p = 1_000_000_007
+factorial = [1 for _ in range(N+1)]
+
+temp = 1
+for i in range(1,N+1):
+    temp = (temp*i)%p
+    factorial[i] = temp
+
+print(factorial)
+
+def pow(bottom,top):
+    if top == 0: return 1
+    elif top == 1: return bottom
+    else:
+        temp = pow(bottom,top//2) % p
+
+        if top%2==0: return (temp * temp) % p # 짝
+        else: return ((bottom % p) * temp * temp % p) # 홀
+
+A = factorial[N]
+B = pow((factorial[N-K] * factorial[K]),p-2)
+answer = (A % p) * (B % p) %p
+print(answer)
