b3f397a6e5e9b7d79298c60eb536f3098b5c9dc4
tgyuu-An <atg0614@gmail.com>
2024-02-05 17:23:23 +0900

    BOJ 13549 숨바꼭질 3 - 0-1 BFS

---
+++ BOJ/BFS/13549_숨바꼭질 3.py
@@ -0,0 +1,29 @@
+from collections import deque, defaultdict
+
+N, K = map(int,input().split())
+
+deq = deque([[N,0]])
+table = defaultdict(lambda : int(1e9))
+
+while deq:
+    now, now_cost = deq.popleft()
+    # print(now, now_cost)
+    
+    if now == K:
+        print(now_cost)
+        break
+    
+    if now*2 <= K:
+        if table[now*2] > now_cost:
+            deq.appendleft([now*2,now_cost])
+            table[now*2] = now_cost
+
+    if now-1 >= 0:
+        if table[now-1] > now_cost:
+            deq.append([now-1,now_cost+1])
+            table[now-1] = now_cost+1
+
+    if now+1 <= K:
+        if table[now+1] > now_cost:
+            deq.append([now+1,now_cost+1])
+            table[now+1] = now_cost+1
