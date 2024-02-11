9212031265a53480c65447863b116819ce1f714b
tgyuu-An <atg0614@gmail.com>
2024-02-12 06:08:39 +0900

    BOJ 3067 Coins - DP

--- BOJ/DP/2293_동전 1.py
+++ BOJ/DP/2293_동전 1.py
@@ -9,7 +9,5 @@ dp_table[0] = 1
 for row, coin in enumerate(coins):
     for column in range(coin,target+1):
         dp_table[column] = dp_table[column] + dp_table[column-coin]
-
-    print(dp_table)
-
+        
 print(dp_table[-1])
---
+++ BOJ/DP/3067_Coins.py
@@ -0,0 +1,17 @@
+import sys
+
+total_test_case = int(input())
+
+for _ in range(total_test_case):
+    coins_type = int(sys.stdin.readline())
+    coins = list(map(int,sys.stdin.readline().split()))
+    target = int(sys.stdin.readline())
+    
+    dp_table = [0 for _ in range(target+1)]
+    dp_table[0] = 1
+
+    for coin in coins:
+        for column in range(1,target+1):
+            dp_table[column] = dp_table[column] + dp_table[column-coin]
+    
+    print(dp_table[-1])
