fb1c949cfd66c9712b8b530e0679e9082873295b
tgyuu-An <atg0614@gmail.com>
2024-02-15 17:17:00 +0900

    BOJ 10844 쉬운 계단 수 - DP

---
+++ BOJ/DP/10844_쉬운 계단 수.py
@@ -0,0 +1,23 @@
+N = int(input())
+
+dp_table = [[0 for _ in range(10)] for _ in range(N+1)]
+
+for i in range(1,10):
+    dp_table[1][i] = 1
+
+print(dp_table)
+
+for step in range(2,N+1):
+    for num in range(10):
+        print(num)
+
+        if num != 0 and num != 9:
+            dp_table[step][num] = dp_table[step-1][num-1] + dp_table[step-1][num+1]
+          
+        elif num == 0:
+            dp_table[step][num] = dp_table[step-1][num+1]
+
+        elif num == 9:
+            dp_table[step][num] = dp_table[step-1][num-1]
+
+print(sum(dp_table[-1]))
