e43a25cec44fa5c03b22972d683acbeb2f3bbf42
tgyuu-An <atg0614@gmail.com>
2024-02-16 02:43:51 +0900

    BOJ 21921 블로그 - 큐

--- BOJ/그래프/1707_이분 그래프.py
+++ BOJ/그래프/1707_이분 그래프.py
@@ -1,46 +1,60 @@
-from collections import defaultdict
+from collections import defaultdict, deque
+import sys
 
-total_test_case = int(input())
-
-answer = False
-
-def dfs(a_set, b_set, graph_information, start_idx):
-    global answer
-
-    if start_idx == len(graph_information):
-        return True
-        pass
-
-    for idx, vertex in enumerate(graph_information[start_idx:]):
-        if vertex in a_set: continue
-
-        a_set.add(vertex)
-        
-        for neighbor_vertex in graph_information[vertex]:
-            if neighbor_vertex in a_set: break
-            
-            b_set.add(neighbor_vertex)
-
-        else: dfs(a_set, b_set, graph_information, start_idx+idx+1)
-
-        a_set.discard(vertex)
-
-        for neighbor_vertex in graph_information[vertex]:
-            b_set.discard(neighbor_vertex)
+total_test_case = int(sys.stdin.readline())
+A = 0
+B = 1
 
 for _ in range(total_test_case):
-    vertex_count, edge_count = map(int,input().split())
+    vertex_count, edge_count = map(int,sys.stdin.readline().split())
+    graph_information = defaultdict(set)
+    answer = True
 
-    graph_information = defaultdict(list)
-
-    for _ in edge_count:
-        start, destination = map(int,input().split())
-        graph_information[start].append(destination)
-        graph_information[destination].append(start)
+    for _ in range(edge_count):
+        start, destination = map(int,sys.stdin.readline().split())
+        graph_information[start].add(destination)
+        graph_information[destination].add(start)
 
     a_set, b_set = set(), set()
+    for vertex in graph_information:
+        if vertex in a_set or vertex in b_set: continue
 
-    dfs(a_set, b_set, graph_information, 0)
+        deq = deque()
 
-    print(graph_information)
-    print(answer)
+        if len(a_set) == 0 and len(b_set) == 0:
+            deq.append([vertex, A])
+            a_set.add(vertex)
+
+        if len(graph_information[vertex] - a_set) == len(graph_information[vertex]):
+            deq.append([vertex, A])
+            a_set.add(vertex)
+ 
+        elif len(graph_information[vertex] - b_set) == len(graph_information[vertex]):
+            deq.append([vertex, B])
+            a_set.add(vertex)
+
+        while deq:
+            now, flag = deq.popleft()
+
+            for neighbor_vertex in graph_information[now]:
+                if flag == A and neighbor_vertex in b_set: continue
+                if flag == B and neighbor_vertex in a_set: continue
+                if flag == A and neighbor_vertex in a_set: 
+                    answer = False
+                    break
+
+                if flag == B and neighbor_vertex in b_set: 
+                    answer = False
+                    break
+                
+                if flag == A:
+                    b_set.add(neighbor_vertex)
+                    deq.append([neighbor_vertex, B])
+
+                elif flag == B:
+                    a_set.add(neighbor_vertex)
+                    deq.append([neighbor_vertex, A])
+
+        if answer == False: break
+    if answer: print("YES")
+    else: print("NO")
---
+++ BOJ/큐/21921_블로그.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+total_blog_day, duration = map(int,input().split())
+
+_max = 0
+max_duration = 0
+deq = deque()
+now = 0
+for today in map(int,input().split()):
+    if len(deq) >= duration:
+        top = deq.popleft()
+        now -= top
+        deq.append(today)
+        now += today
+
+    elif len(deq) < duration:
+        now += today
+        deq.append(today)
+
+    if _max < now:
+        _max = now
+        max_duration = 1
+
+    elif _max == now:
+        max_duration += 1
+
+if _max == 0:
+    print("SAD")
+else:
+    print(_max)
+    print(max_duration)
