d79fad06c5f2a94cc902c75f3dff00c3b9609f1b
tgyuu-An <atg0614@gmail.com>
2024-02-26 00:09:05 +0900

    BOJ 15927 회문은 회문아니야!! - 애드 혹

---
+++ BOJ/애드 혹/15927_회문은 회문아니야!!.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+string = input()
+
+if string != string[::-1]:
+    print(len(string))
+
+elif string[1:] == string[:0:-1]:
+    print(-1)
+
+else: print(len(string)-1)
