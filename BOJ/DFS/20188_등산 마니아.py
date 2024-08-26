7076174a4442e5af7f8ca156e7e5478aa0a9687c
tgyuuAn <atg0614@gmail.com>
2024-08-27 04:30:57 +0900

    BOJ 14324 Rain (Small) - BFS

--- BOJ/DFS/20188_등산 마니아.py
+++ BOJ/DFS/20188_등산 마니아.py
@@ -1,15 +1,31 @@
-from itertools import permutations
 import sys
+sys.setrecursionlimit(10 ** 6)
 
 def input(): return sys.stdin.readline().rstrip()
 
-tree = [[] for _ in range(N)]
 N = int(input())
+# N = 30 만 -> O(N*log(N))
+
+tree = [[] for _ in range(N+1)]
 for _ in range(N-1):
     node1, node2 = map(int, input().split())
     tree[node1].append(node2)
     tree[node2].append(node1)
 
-for x, y in permutations(range(N), 2):
-    print(x, y)
+answer = 0
+def dfs(now_idx, visited, tree):
+    global answer
     
+    temp_cnt = 1
+    for next_node in tree[now_idx]:
+        if next_node in visited: continue
+        visited.add(next_node)
+        temp = dfs(next_node, visited, tree)
+        answer += temp * (temp-1) // 2
+        answer += temp * (N-temp)
+        temp_cnt += temp
+
+    return temp_cnt
+
+dfs(1,{1,}, tree)
+print(answer)    
